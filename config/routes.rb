dfea3e5ab6c7fb2976435814353a34abe15265b9
鈴木晃一 <suzukikouichi@Suzuki.local>
2025-06-14 21:54:01 +0900

    Google認証の下準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root "articles#index"
-  get "/articles", to: "articles#index"
+  get "static_pages/before_login"
+  get "static_pages/after_login"
+  root 'static_pages#before_login'
+  get '/after_login', to: 'static_pages#after_login'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
